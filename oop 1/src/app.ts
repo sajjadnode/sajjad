7215e9cabac11a62949ebdc5676e73d1acb87aaf
sajjadnode <96042045+sajjadnode@users.noreply.github.com>
2021-12-17 11:34:17 +0330

    this is exercise of Oop programming

---
+++ oop 1/src/app.ts
@@ -0,0 +1,48 @@
+class animal {
+    name: string;
+    walkType: string;
+    sound: string;
+
+    constructor(name: string, walkType: string ,sound:string){
+            this.name = name 
+            this.walkType = walkType
+            this.sound = sound
+    }
+
+    move(){
+        console.log(this.name, this.walkType,"with sound",this.sound )
+    }
+
+}
+
+class Snake extends animal{
+   constructor(){
+       super('snake', 'crawl','sound.mp3')
+   }
+
+  
+}
+class Dog extends animal{
+
+    constructor(){
+        super('dog', 'walk','sound.mp3')
+    }
+ 
+}
+class Birt extends animal{
+ 
+    constructor(){
+        super('birt', 'fly','sound.wav')
+    }
+ 
+}
+
+
+const s = new Snake();
+s.move()
+
+const d = new Dog();
+d.move()
+
+const b = new Birt()
+b.move();
